e3c0604473a58dea4b37fa51ddf09cd2519727bb
Tetsushiro Fujita <googlycine.philosophique@gmail.com>
2012-07-01 22:50:52 +0900

    [refactor] p.9 コードを正しくする。

--- src/main/java/my/study/tdd/Dollar.java
+++ src/main/java/my/study/tdd/Dollar.java
@@ -2,15 +2,14 @@ package my.study.tdd;
 
 public class Dollar {
 
-	int amount = 10;
+	int amount = 5 * 2;
 
-	public Dollar(int i) {
-		// TODO Auto-generated constructor stub
+	public Dollar(int amount) {
+		this.amount = amount;
 	}
 
-	void times(int i) {
-		// TODO Auto-generated method stub
-		
+	void times(int multiplier) {
+		amount *= multiplier;
 	}
 
 }
