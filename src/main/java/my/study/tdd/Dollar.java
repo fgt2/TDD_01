c97ede00e416bd3db7cfc985539b88e1f59df887
Tetsushiro Fujita <googlycine.philosophique@gmail.com>
2012-07-01 23:17:03 +0900

    [green] p.13 Dollarをimmutableに変更。

--- src/main/java/my/study/tdd/Dollar.java
+++ src/main/java/my/study/tdd/Dollar.java
@@ -8,9 +8,8 @@ public class Dollar {
 		this.amount = amount;
 	}
 
-	void times(int multiplier) {
-		amount *= multiplier;
-		
+	Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
 	}
 
 }
