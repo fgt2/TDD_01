1e299b6b9760dd7cb928b08d25e3528a8783f658
Tetsushiro Fujita <googlycine.philosophique@gmail.com>
2012-07-01 22:43:59 +0900

    [green] p.6 テストをパスさせるための最も小さな変更

---
+++ src/main/java/my/study/tdd/Dollar.java
@@ -0,0 +1,16 @@
+package my.study.tdd;
+
+public class Dollar {
+
+	int amount = 10;
+
+	public Dollar(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	void times(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
